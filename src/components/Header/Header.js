0459c04decec83c45196cce19098d52481ca3427
sudipto68 <sudiptomitro2016@gmail.com>
2021-03-04 00:03:52 +0600

    first commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import logo from "../../images/logo.png";
+
+const Header = () => {
+  return (
+    <div>
+      <img src={logo} alt="company logo" />
+    </div>
+  );
+};
+
+export default Header;
