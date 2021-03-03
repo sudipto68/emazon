8d29cf312c4b42f3d702cbc2c0ca69b948de0255
sudipto68 <sudiptomitro2016@gmail.com>
2021-03-04 00:22:54 +0600

    new component added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,16 @@
 import React from "react";
+import "./Header.css";
 import logo from "../../images/logo.png";
 
 const Header = () => {
   return (
-    <div>
+    <div className="header">
       <img src={logo} alt="company logo" />
+      <nav>
+        <a href="/shop">Shop</a>
+        <a href="/review">Order Review</a>
+        <a href="/manage">Manage Inventory</a>
+      </nav>
     </div>
   );
 };
